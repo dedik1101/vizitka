ae55a0598461a4704f62a0a5c61557ccfc48019c
dedik1101 <81088645+dedik1101@users.noreply.github.com>
2025-04-13 00:17:37 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
 	reactStrictMode: false,
 	output: 'export',
+	basePath: process.env.NODE_ENV === 'production' ? '/vizitka' : '',
 	images: {
 	    unoptimized: true,
 	    domains: ['www.eao.ru', 'upload.wikimedia.org'],
