b70f0bf2b62d1ca09a3b637f19b5c4026679268c
dedik1101 <81088645+dedik1101@users.noreply.github.com>
2025-04-13 00:10:01 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
 	reactStrictMode: false,
 	output: 'export',
+	images: {
+	    unoptimized: true,
+	    domains: ['www.eao.ru', 'upload.wikimedia.org'],
+	},
 };
 
 module.exports = nextConfig;
